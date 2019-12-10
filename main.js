762dd0893ac6c74f0b5ab2f4c4cc933315f86817
phenix1229 <paul.garay@codeimmersives.com>
2019-12-10 15:32:44 -0500

    almost done

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
 class Vehicle {
+  constructor(name, wheelCount, maxSpeed){
+    this.name = name;
+    this.wheelCount = wheelCount;
+    this.maxSpeed = maxSpeed;
+    this.speed = 0;
+  }
+
+    accelerate() {
+      if (this.speed < this.maxSpeed){
+      this.speed += 40;
+      }
+    };
 }
 
-class Car {
+class Car extends Vehicle{
+  constructor(name){
+    super(name, 4)
+  }
+
+  drive() {
+    return 'Vroom. '
+  }
+
 }
 
-class Ferrari {
+class Ferrari extends Car{
+  constructor(){
+    super()
+    this.name = 'Ferrari';
+    this.maxSpeed = 180;
+  }
+
+  accelerate(){
+    if (this.speed < this.maxSpeed){
+      this.speed += 90;
+    }
+  }
 }
 
 
